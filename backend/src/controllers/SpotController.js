95caf5c42f0a0d1f28d05bb92e56035f6ca47664
SamuelHiroyuki <samduasvezesmortal@gmail.com>
2019-10-01 22:09:35 -0300

    Upload de arquivos

---
+++ backend/src/controllers/SpotController.js
@@ -0,0 +1,18 @@
+const Spot = require("../models/Spot");
+
+module.exports = {
+	async store(req, res) {
+		// const { email } = req.body;
+
+		// let user = await User.findOne({ email });
+
+		// if (!user) {
+		// 	user = await User.create({ email });
+		// }
+
+		// return res.json(user);
+		// console.log(req.body);
+		// console.log(req.file);
+		return res.json({});
+	}
+};
