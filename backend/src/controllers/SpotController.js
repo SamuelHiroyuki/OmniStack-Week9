c50a3c1965aed0d349b5506e95de1fbbe68d151f
SamuelHiroyuki <samduasvezesmortal@gmail.com>
2019-10-01 22:25:29 -0300

    Spot controller

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,18 +1,36 @@
+const User = require("../models/User");
 const Spot = require("../models/Spot");
 
 module.exports = {
+	async index(req, res) {
+		const { tech } = req.query;
+
+		const spots = await Spot.find({ techs: tech });
+
+		return res.json(spots);
+	},
 	async store(req, res) {
-		// const { email } = req.body;
+		const { filename: thumbnail } = req.file;
+		const { company, techs, price } = req.body;
+		const { user_id } = req.headers;
 
-		// let user = await User.findOne({ email });
+		try {
+			const user = await User.findById(user_id);
+			if (!user) {
+				return res.status(400).json({ error: "User does not exists!" });
+			}
+		} catch (error) {
+			return res.status(400).json({ error: "User does not exists!" });
+		}
 
-		// if (!user) {
-		// 	user = await User.create({ email });
-		// }
+		const spot = await Spot.create({
+			thumbnail,
+			company,
+			techs: techs.split(",").map(t => t.trim()),
+			price,
+			user: user_id
+		});
 
-		// return res.json(user);
-		// console.log(req.body);
-		// console.log(req.file);
-		return res.json({});
+		return res.json(spot);
 	}
 };
