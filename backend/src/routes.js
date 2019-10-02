0a6e135fb206253c7f0abde2c4765d30ac2649b3
SamuelHiroyuki <samduasvezesmortal@gmail.com>
2019-10-01 22:29:36 -0300

    Dashboard controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,11 +4,15 @@ const multer = require("multer");
 const uploadConfig = require("./config/upload");
 const SessionController = require("./controllers/SessionController");
 const SpotController = require("./controllers/SpotController");
+const DashboardController = require("./controllers/DashboardController");
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
 routes.post("/sessions", SessionController.store);
+
+routes.get("/dashboards", DashboardController.show);
+
 routes.get("/spots", SpotController.index);
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 
